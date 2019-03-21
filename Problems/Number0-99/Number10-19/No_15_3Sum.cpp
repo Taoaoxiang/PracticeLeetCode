a2fe7beb766cc4331f48dae25439e3ef3695d3e1
OliverTao <taoaoxi@isu.edu>
2019-03-21 06:00:02 -0700

    Update No_15_3Sum.cpp

--- Problems/Number0-99/Number10-19/No_15_3Sum.cpp
+++ Problems/Number0-99/Number10-19/No_15_3Sum.cpp
@@ -1,3 +1,4 @@
 // This name 3Sum is apparently for adult who's playing wild.
 // :P
 //im writing this line on my phone,lol
+//im @ La Pine State Park, and its raining outside. 6:00am, still in bed. barely have reception.
