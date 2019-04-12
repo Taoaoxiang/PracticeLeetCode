9935186f049b02669db24ef727cb4154658df8df
OliverTao <taoaoxi@isu.edu>
2019-04-12 16:27:06 -0600

    finish #92, two ways

---
+++ Problems/Number0-99/Number90-99/No_92_Reverse-Linked-List-II.cpp
@@ -0,0 +1,99 @@
+﻿//Reverse a linked list from position m to n.Do it in one - pass.
+//
+//Note: 1 ≤ m ≤ n ≤ length of list.
+//
+//	Example :
+//
+//	Input : 1->2->3->4->5->NULL, m = 2, n = 4
+//	Output : 1->4->3->2->5->NULL
+//
+//
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+//Runtime: 4 ms, faster than 100.00% of C++ online submissions for Reverse Linked List II.
+//Memory Usage : 8.5 MB, less than 100.00% of C++ online submissions for Reverse Linked List II.
+
+// This problem asks for one-pass solution.
+// This solution works. :)
+class Solution {
+public:
+	ListNode* reverseBetween(ListNode* head, int m, int n) {
+		if (m == n) { return head; }
+		int i = 1;
+		// Left: Head->End
+		// Mid: Head->End
+		// Right: Head
+		ListNode *p = head, *pLEnd = NULL, *pMHead = NULL, *pCur = NULL, *pRHead = NULL;
+		while (i <= n) {
+			if (i < m - 1) { p = p->next; }
+			else if (i == m - 1) {
+				pLEnd = p;
+				p = p->next;
+			}
+			else if (i == m) {
+				pRHead = p;
+				pMHead = p;
+				p = p->next;
+			}
+			else if (i > m && i < n) {
+				pCur = p;
+				p = p->next;
+				pCur->next = pMHead;
+				pMHead = pCur;
+			}
+			else if (i == n) {
+				pRHead->next = p->next;
+				pCur = p;
+				if (pLEnd != NULL) { pLEnd->next = pCur; }
+				pCur->next = pMHead;
+				pMHead = pCur;
+			}
+			++i;
+		}
+		if (pLEnd == NULL) { return pMHead; }
+		else { return head; }
+	}
+};
+
+
+//Runtime: 4 ms, faster than 100.00% of C++ online submissions for Reverse Linked List II.
+//Memory Usage : 8.7 MB, less than 98.13% of C++ online submissions for Reverse Linked List II.
+
+class Solution {
+public:
+	ListNode* reverseBetween(ListNode* head, int m, int n) {
+		if (m == n) { return head; }
+		int i = 0;
+		ListNode *p = head, *pTmp;
+		vector<ListNode*> vec;
+		while (p != NULL) {
+			vec.push_back(p);
+			p = p->next;
+		}
+		for (int iL = m - 1, iR = n - 1; iL < iR; ++iL, --iR) {
+			pTmp = vec[iL];
+			vec[iL] = vec[iR];
+			vec[iR] = pTmp;
+		}
+		for (int i = 0; i < vec.size(); ++i) {
+			if (i + 1 < vec.size()) { vec[i]->next = vec[i + 1]; }
+			else { vec[i]->next = NULL; }
+		}
+
+		return vec[0];
+	}
+};
--- test.cpp
+++ test.cpp
@@ -8,6 +8,66 @@
 
 using namespace std;
 
+class Solution {
+public:
+	int numDecodings(string s) {
+		
+		int sSize = s.size();
+		if (sSize == 0) { return 0; }
+		else if (sSize == 1) { 
+			if (s[0] == '0') { return 0; } 
+			else { return 1; }
+		}
+		int rtn = 0;
+		for (int i = 0; i < sSize; ++i) {
+			if (i == 0) {
+				if (s[i] == '0') { return 0; }
+				else if (s[i] == '1') { rtn += 2; }
+				else if (s[i] == '2') { 
+					if (s[i + 1] >= '0' && s[i + 1] <= '6') { rtn += 2; }
+					else { rtn += 1; }
+				}
+				else { rtn += 1; }
+			}
+			else if (i == sSize - 1) {
+				if (s[i] == '0') {
+					if (s[i - 1] == '1' || s[i - 1] == '2') { rtn += 1; }
+					else { return 0; }
+				}
+				else {
+					if (s[i - 1] == '1') { rtn += 2; }
+					else if (s[i - 1] == '2' && (s[i] >= '1'&&s[i] <= '6')) { rtn += 2; }
+					else { rtn += 1; }
+				}
+			}
+			else {
+				if (s[i] == '0') {
+					if (s[i - 1] == '1' || s[i - 1] == '2') { rtn += 1; }
+					else { return 0; }
+				}
+				else if (s[i] == '1') { 
+					if (s[i - 1] == '1' || s[i - 1] == '2') { rtn += 3; }
+					else { rtn += 2; }
+				}
+				else if (s[i] == '2') {
+					if (s[i - 1] == '1' || s[i - 1] == '2') { rtn += 1; }
+					if (s[i + 1] >= '0' && s[i + 1] <= '6') { rtn += 1; }
+					rtn += 1;
+				}
+				else { 
+					if (s[i - 1] == '1' ) { rtn += 2; } 
+					else if (s[i - 1] == '2' && (s[i]>='1'&&s[i] <= '6')) { rtn += 2; }
+					else { rtn += 1; }
+				}
+			}
+		}
+		return rtn-sSize-1;
+	}
+};
+
+
+
+
 
 
 int main() {
